38e0452ac29be1d41b447d9162bf8c55e983710f
ien646 <1340246+ien646@users.noreply.github.com>
2025-05-19 00:00:00 +0000

    add relative motion input events while pointer is locked

--- src/LockPointer.cpp
+++ src/LockPointer.cpp
@@ -1,3 +1,8 @@
+#include "WaylandQtPointerConstraints/LockPointer.h"
+#include "WaylandQtPointerConstraints/LockPointer.h"
+#include "WaylandQtPointerConstraints/LockPointer.h"
+#include "WaylandQtPointerConstraints/LockPointer.h"
+
 #include <WaylandQtPointerConstraints/LockPointer.h>
 
 #include <QGuiApplication>
@@ -16,14 +21,32 @@ LockPointer::LockPointer(QWindow* window)
     _registryListener.global = [](void* data, wl_registry* registry, const uint32_t name, const char* interface,
                                   const uint32_t version)
     {
+        auto* _this = static_cast<LockPointer*>(data);
         if (std::string_view(interface) == std::string_view(zwp_pointer_constraints_v1_interface.name))
         {
-            *static_cast<zwp_pointer_constraints_v1**>(data) = static_cast<zwp_pointer_constraints_v1*>(
+            _this->_pointerConstraintsV1 = static_cast<zwp_pointer_constraints_v1*>(
                 wl_registry_bind(
                     registry, name, &zwp_pointer_constraints_v1_interface, version));
         }
+        else if (std::string_view(interface) == zwp_relative_pointer_manager_v1_interface.name)
+        {
+            _this->_relativePointerManagerV1 = static_cast<zwp_relative_pointer_manager_v1*>(wl_registry_bind(
+                registry, name, &zwp_relative_pointer_manager_v1_interface, version));
+        }
     };
     _registryListener.global_remove = nullptr;
+
+    _relativePointerListenerV1.relative_motion = [](void* data, [[maybe_unused]] zwp_relative_pointer_v1* rpointer,
+                                                    [[maybe_unused]] uint32_t utime_hi,
+                                                    [[maybe_unused]] uint32_t utime_lo,
+                                                    [[maybe_unused]] const wl_fixed_t delta_x,
+                                                    [[maybe_unused]] const wl_fixed_t delta_y,
+                                                    wl_fixed_t delta_x_unaccel,
+                                                    wl_fixed_t delta_y_unaccel)
+    {
+        auto* _this = static_cast<LockPointer*>(data);
+        _this->emitMouseMovementDelta(QPoint{wl_fixed_to_int(delta_x_unaccel), wl_fixed_to_int(delta_y_unaccel)});
+    };
 
     _nativeInterface = QGuiApplication::platformNativeInterface();
     _compositor = static_cast<wl_compositor*>(_nativeInterface->nativeResourceForIntegration("compositor"));
@@ -59,9 +82,12 @@ void LockPointer::lockPointer(const QRect region)
     auto* pointer = static_cast<wl_pointer*>(_nativeInterface->nativeResourceForIntegration("wl_pointer"));
 
     auto* registry = wl_display_get_registry(_display);
-    wl_registry_add_listener(registry, &_registryListener, &_pointerConstraintsV1);
+    wl_registry_add_listener(registry, &_registryListener, this);
 
     wl_display_roundtrip(_display);
+
+    _relativePointerV1 = zwp_relative_pointer_manager_v1_get_relative_pointer(_relativePointerManagerV1, pointer);
+    zwp_relative_pointer_v1_add_listener(_relativePointerV1, &_relativePointerListenerV1, this);
 
     if (_lockedRegion)
     {
@@ -98,6 +124,14 @@ void LockPointer::unlockPointer()
 bool LockPointer::isPointerLocked() const
 {
     return _locked;
+}
+
+void LockPointer::emitMouseMovementDelta(const QPoint delta)
+{
+    if (_locked && delta.x() > 0 && delta.y() > 0)
+    {
+        emit mouseMovementDelta(delta);
+    }
 }
 
 #ifdef WAYLAND_QT_POINTER_CONSTRAINTS_CUSTOM_NAMESPACE
