b8d8b0116f46e313b7bac734573f40a19f4ac68c
ien <ien@amsostudios.com>
2025-05-21 14:00:07 +0200

    initial commit

---
+++ LockPointer.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <QWidget>
+
+#include <pointer-constraints.h>
+
+class QPlatformNativeInterface;
+
+#ifdef WAYLAND_QT_POINTER_CONSTRAINTS_CUSTOM_NAMESPACE
+namespace WAYLAND_QT_POINTER_CONSTRAINTS_CUSTOM_NAMESPACE
+{
+#endif
+
+    class LockPointer
+    {
+    public:
+        explicit LockPointer(QWindow* window);
+
+        void lockPointer(QRect region);
+        void unlockPointer();
+
+        [[nodiscard]] bool isPointerLocked() const;
+
+    private:
+        QWindow* _window = nullptr;
+        bool _locked = false;
+        QPlatformNativeInterface* _nativeInterface = nullptr;
+        wl_compositor* _compositor = nullptr;
+        wl_display* _display = nullptr;
+        zwp_pointer_constraints_v1* _pointerConstraintsV1 = nullptr;
+        zwp_locked_pointer_v1* _lockedPointerV1 = nullptr;
+        wl_region* _lockedRegion = nullptr;
+        wl_registry_listener _registryListener = {};
+    };
+
+#ifdef WAYLAND_QT_POINTER_CONSTRAINTS_CUSTOM_NAMESPACE
+}
+#endif
---
+++ examples/LockingButton.cpp
@@ -0,0 +1,82 @@
+#include <QApplication>
+#include <QHBoxLayout>
+#include <QLabel>
+#include <QMainWindow>
+#include <QPushButton>
+
+#include "../LockPointer.h"
+
+class ConfiningButton final : public QPushButton
+{
+public:
+    explicit ConfiningButton(QWidget* parentWidget)
+        : QPushButton(parentWidget)
+    {
+        setText("Freeze!");
+
+    }
+
+private:
+    std::unique_ptr<LockPointer> _lockPointer;
+    QWidget* _nativeWidget = nullptr;
+
+    void mousePressEvent(QMouseEvent* event) override
+    {
+        if (!_nativeWidget)
+        {
+            if (windowHandle())
+            {
+                _nativeWidget = this;
+            }
+            else if (nativeParentWidget())
+            {
+                _nativeWidget = nativeParentWidget();
+            }
+            else
+            {
+                // No native parent found
+                exit(-1);
+            }
+        }
+
+        if (!_lockPointer)
+        {
+            _lockPointer = std::make_unique<LockPointer>(_nativeWidget->windowHandle());
+        }
+
+        if (!_lockPointer->isPointerLocked())
+        {
+            const QPoint lockPos = mapTo(_nativeWidget, QPoint{0, 0});
+            _lockPointer->lockPointer(QRect{lockPos, QSize{width(), height()}});
+        }
+        else
+        {
+            _lockPointer->unlockPointer();
+        }
+    }
+};
+
+class MainWindow final : public QMainWindow
+{
+public:
+    MainWindow()
+    {
+        const auto centralWidget = new QWidget(this);
+        const auto layout = new QHBoxLayout(this);
+        layout->addWidget(new QLabel("Not here"));
+        layout->addWidget(new ConfiningButton(this));
+        layout->addWidget(new QLabel("Not here"));
+        centralWidget->setLayout(layout);
+        setCentralWidget(centralWidget);
+    }
+};
+
+int main(int argc, char** argv)
+{
+    QApplication app(argc, argv);
+
+    MainWindow win;
+    win.show();
+
+    return app.exec();
+}
