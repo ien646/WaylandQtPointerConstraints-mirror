99fb0f9afc6c76a0abcc0b118d19870cfcf0b6da
ien <ien@amsostudios.com>
2025-05-21 21:59:58 +0200

    add relative motion input events while pointer is locked

--- generate-protocols.sh
+++ generate-protocols.sh
@@ -5,3 +5,6 @@ mkdir generated-protocols
 
 wayland-scanner client-header wayland-protocols/unstable/pointer-constraints/pointer-constraints-unstable-v1.xml generated-protocols/pointer-constraints.h
 wayland-scanner private-code wayland-protocols/unstable/pointer-constraints/pointer-constraints-unstable-v1.xml generated-protocols/pointer-constraints.c
+
+wayland-scanner client-header wayland-protocols/unstable/relative-pointer/relative-pointer-unstable-v1.xml generated-protocols/relative-pointer.h
+wayland-scanner private-code wayland-protocols/unstable/relative-pointer/relative-pointer-unstable-v1.xml generated-protocols/relative-pointer.c
